c2cdec96c96d13ededd0daf7e5b37e7bf90aaa95
Vyacheslav <stormfall228@gmai.com>
2025-03-24 18:39:08 +0500

    asd

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,10 @@
-module.exports = {
-    projectId: "ipvy1a",
-    // ...rest of the Cypress project config
-}
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
